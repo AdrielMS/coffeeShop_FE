441a132ce6a1c1a553850a3f898964f275a32414
AdrielMS <adrielsjahranie@gmail.com>
2023-03-17 14:56:31 +0700

    modified:   src/pages/productAdmin/index.js

--- src/pages/productAdmin/index.js
+++ src/pages/productAdmin/index.js
@@ -156,7 +156,7 @@ const ProductsAdmin = (e) => {
                       <img
                         className="rounded-full product-images "
                         // src={`http://localhost:5000/upload/images/${item.images[0].filename}`}
-                        src={`https://coffeeshop.cyclic.app/upload/images${item.images[0].filename}`}
+                        src={`https://coffeeshop.cyclic.app/upload/images/${item.images[0].filename}`}
                         alt=""
                       />
                     </Link>
